8788c359fc81bec0b787a4c61f6b64f9c9bd15cc
Benedikt Haas <benedikt.haas@ika.rwth-aachen.de>
2023-04-12 11:49:56 +0200

    move ROS version dependent apt pkgs to custom.sh

--- docker/custom.sh
+++ docker/custom.sh
@@ -1,4 +1,19 @@
 export DOCKER_ROS_FILES_PATH=/docker-ros/files
+# Install ROS version dependent apt packages
+if [ "$ROS_VERSION" = "2" ]; then
+    ADDITIONAL_PACKAGES="ros-$ROS_DISTRO-rviz2"
+else
+    ADDITIONAL_PACKAGES="ros-$ROS_DISTRO-rviz
+                         ros-$ROS_DISTRO-opencv-apps
+                         ros-$ROS_DISTRO-rospy
+                         ros-$ROS_DISTRO-rospy-message-converter
+                         ros-$ROS_DISTRO-pcl-ros
+                         ros-$ROS_DISTRO-derived-object-msgs
+                         python3-catkin-tools
+                         python3-catkin-pkg
+                         python3-catkin-pkg-modules"
+fi
+apt-get install --no-install-recommends -y $ADDITIONAL_PACKAGES
 # Install Python dependencies
 pip$PYTHON_SUFFIX install --upgrade pip$PYTHON_SUFFIX
 pip$PYTHON_SUFFIX install -r $DOCKER_ROS_FILES_PATH/requirements.txt
