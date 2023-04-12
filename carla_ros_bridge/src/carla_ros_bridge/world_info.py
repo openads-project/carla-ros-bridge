ad03d00e2769c74f6ba9b9f5681a7f2f64036268
Christian Geller <christian.geller@ika.rwth-aachen.de>
2023-04-12 17:05:33 +0200

    fix TransformBroadcaster for ros2

--- carla_ros_bridge/src/carla_ros_bridge/world_info.py
+++ carla_ros_bridge/src/carla_ros_bridge/world_info.py
@@ -46,7 +46,7 @@ class WorldInfo(object):
             qos_profile=QoSProfile(depth=10, durability=DurabilityPolicy.TRANSIENT_LOCAL))
 
 
-        self._tf_broadcaster = tf2_ros.TransformBroadcaster()
+        self._tf_broadcaster = tf2_ros.TransformBroadcaster(self)
 
     def destroy(self):
         """
