ac70f7397357e482564479842921bb8263bee31b
Christian Geller <christian.geller@ika.rwth-aachen.de>
2023-04-19 14:33:39 +0200

    restore version

--- carla_ros_bridge/setup.py
+++ carla_ros_bridge/setup.py
@@ -19,7 +19,7 @@ elif ROS_VERSION == 2:
     package_name = 'carla_ros_bridge'
     setup(
         name=package_name,
-        version='0.9.14',
+        version='0.0.0',
         packages=[package_name],
         data_files=[('share/ament_index/resource_index/packages', ['resource/' + package_name]),
                     ('share/' + package_name, ['package.xml']),
