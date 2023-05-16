079f433c9e1055f1bcff69a038c183c304523bc3
Christian Geller <christian.geller@ika.rwth-aachen.de>
2023-05-16 14:30:07 +0000

    fix artifact extraction

--- docker/custom.sh
+++ docker/custom.sh
@@ -17,15 +17,15 @@ apt-get install --no-install-recommends -y $ADDITIONAL_PACKAGES
 pip$PYTHON_SUFFIX install --upgrade pip$PYTHON_SUFFIX
 pip$PYTHON_SUFFIX install -r $DOCKER_ROS_FILES_PATH/requirements.txt
 # Check if user provided CARLA PythonAPI. If not, download it as artifact from CARLA CI pipeline
-mkdir -p /opt/carla/PythonAPI
-if [ -d "$DOCKER_ROS_FILES_PATH/carla" ]; then
-    mv $DOCKER_ROS_FILES_PATH/carla /opt/carla/PythonAPI
+mkdir -p /opt/carla
+if [ -d "$DOCKER_ROS_FILES_PATH/PythonAPI" ]; then
+    mv $DOCKER_ROS_FILES_PATH/PythonAPI /opt/carla/PythonAPI
 else
-    mkdir -p /opt/carla/PythonAPI/carla
+    mkdir -p /opt/carla
      curl --location --output artifacts.zip "https://gitlab.ika.rwth-aachen.de/api/v4/projects/1645/jobs/artifacts/develop/download?job=carla:extract_artifacts&job_token=$GIT_HTTPS_PASSWORD"
     unzip artifacts.zip -d /opt/carla/
 fi
 # Create a script to append necessary paths to PYTHONPATH and make .bashrc source it
-echo "export PYTHONPATH=\$PYTHONPATH:/opt/carla/PythonAPI/carla/dist/$(ls /opt/carla/PythonAPI/carla/dist | grep py$ROS_PYTHON_VERSION.)" >> /opt/carla/setup.bash
-echo "export PYTHONPATH=\$PYTHONPATH:/opt/carla/PythonAPI/carla" >> /opt/carla/setup.bash
+echo "export PYTHONPATH=\$PYTHONPATH:/opt/carla/PythonAPI/PythonAPI/carla/dist/$(ls /opt/carla/PythonAPI/PythonAPI/carla/dist | grep py$ROS_PYTHON_VERSION.)" >> /opt/carla/setup.bash
+echo "export PYTHONPATH=\$PYTHONPATH:/opt/carla/PythonAPI/PythonAPI/carla" >> /opt/carla/setup.bash
 echo "source /opt/carla/setup.bash" >> /root/.bashrc
