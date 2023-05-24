78eeda43582d369fcf83fe756a3ea3904b68304b
Benedikt Haas <benedikt.haas@ika.rwth-aachen.de>
2023-05-24 11:06:46 +0200

    adjust to changed pythonapi artifact extraction

--- docker/custom.sh
+++ docker/custom.sh
@@ -23,9 +23,11 @@ if [ -d "$DOCKER_ROS_FILES_PATH/PythonAPI" ]; then
 else
     mkdir -p /opt/carla
      curl --location --output artifacts.zip "https://gitlab.ika.rwth-aachen.de/api/v4/projects/1645/jobs/artifacts/develop/download?job=carla:extract_artifacts&job_token=$GIT_HTTPS_PASSWORD"
-    unzip artifacts.zip -d /opt/carla/
+    unzip artifacts.zip
+    mv artifacts_ci/PythonAPI /opt/carla
+    rm -rf artifacts_ci
 fi
 # Create a script to append necessary paths to PYTHONPATH and make .bashrc source it
-echo "export PYTHONPATH=\$PYTHONPATH:/opt/carla/PythonAPI/PythonAPI/carla/dist/$(ls /opt/carla/PythonAPI/PythonAPI/carla/dist | grep py$ROS_PYTHON_VERSION.)" >> /opt/carla/setup.bash
-echo "export PYTHONPATH=\$PYTHONPATH:/opt/carla/PythonAPI/PythonAPI/carla" >> /opt/carla/setup.bash
+echo "export PYTHONPATH=\$PYTHONPATH:/opt/carla/PythonAPI/carla/dist/$(ls /opt/carla/PythonAPI/carla/dist | grep py$ROS_PYTHON_VERSION.)" >> /opt/carla/setup.bash
+echo "export PYTHONPATH=\$PYTHONPATH:/opt/carla/PythonAPI/carla" >> /opt/carla/setup.bash
 echo "source /opt/carla/setup.bash" >> /root/.bashrc
