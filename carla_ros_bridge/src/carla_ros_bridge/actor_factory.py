6847c34a0498bd11de9ca4556db6297d9f0e3c45
Lukas Ostendorf <lukas.ostendorf@ika.rwth-aachen.de>
2023-07-14 07:53:27 +0000

    made name changes only apply to sensors

--- carla_ros_bridge/src/carla_ros_bridge/actor_factory.py
+++ carla_ros_bridge/src/carla_ros_bridge/actor_factory.py
@@ -250,13 +250,9 @@ class ActorFactory(object):
         if parent is not None:
             parent_id = parent.uid
 
-        if not carla_actor.attributes.get("role_name", ""):
+        name = carla_actor.attributes.get("role_name", "")
+        if not name:
             name = str(carla_actor.id)
-        else:
-            # construct unique name
-            actor_type = carla_actor.type_id.replace(".", "_")
-            actor_id = carla_actor.id
-            name = f"{actor_type}_{actor_id}"
 
         obj = self._create_object(carla_actor.id, carla_actor.type_id, name,
                                   parent_id, relative_transform, carla_actor)
