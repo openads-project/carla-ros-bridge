fbb2b9542f434f4836d29f9753964b93a92ef9c7
Lukas Ostendorf <lukas.ostendorf@ika.rwth-aachen.de>
2023-08-14 10:09:46 +0200

    change naming behaviour

--- carla_ros_bridge/src/carla_ros_bridge/sensor.py
+++ carla_ros_bridge/src/carla_ros_bridge/sensor.py
@@ -96,6 +96,13 @@ class Sensor(Actor):
         self.is_event_sensor = is_event_sensor
         self._callback_active = Lock()
 
+        # prevent similar names and thus doupled topics
+        sensor_type = carla_actor.type_id.split(".")[-1]
+        sensor_role_name = carla_actor.attributes.get("role_name", None)
+        self.name = sensor_type
+        if sensor_role_name:
+            self.name += "_" + sensor_role_name
+
         try:
             self.sensor_tick_time = float(carla_actor.attributes["sensor_tick"])
             node.logdebug("Sensor tick time is {}".format(self.sensor_tick_time))
