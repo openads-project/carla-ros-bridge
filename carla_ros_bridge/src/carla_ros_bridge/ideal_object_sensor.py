3afe1040c041b300d366ee9d722f69ad3c18022d
Nils Frobel <nils.frobel@ika.rwth-aachen.de>
2024-04-23 17:53:27 +0200

    add detection of target pose in sensor frame

--- carla_ros_bridge/src/carla_ros_bridge/ideal_object_sensor.py
+++ carla_ros_bridge/src/carla_ros_bridge/ideal_object_sensor.py
@@ -14,7 +14,7 @@ from carla_ros_bridge.walker import Walker
 
 from derived_object_msgs.msg import ObjectArray
 
-from geometry_msgs.msg import Pose, Quaternion
+from geometry_msgs.msg import Pose, Quaternion, TransformStamped
 
 from carla_ros_bridge.object_sensor import ObjectSensor
 
@@ -30,7 +30,14 @@ import ros_compatibility as roscomp
 
 import tf2_ros
 
-from tf_transformations import euler_from_quaternion, quaternion_from_euler
+from tf2_geometry_msgs import do_transform_pose
+
+try:
+    from tf_transformations import euler_from_quaternion, quaternion_from_euler
+    # import tf_transformations
+except ImportError:
+    from tf.transformations import euler_from_quaternion, quaternion_from_euler
+# from inspect import getmembers, isfunction
 
 import math
 import numpy as np
@@ -64,7 +71,7 @@ class IdealObjectSensor(ObjectSensor):
         :param attributes: attributes of IdealObjectSensor
         :type attributes: diagnostic_msgs/KeyValue[]
         """
-        
+        # print(getmembers(tf_transformations, isfunction))
         super(IdealObjectSensor, self).__init__(uid=uid,
                                                       name=name,
                                                       parent=parent,
@@ -86,7 +93,7 @@ class IdealObjectSensor(ObjectSensor):
         # Extract (relative) spawn pose
         self.relative_spawn_pose = relative_spawn_pose
         # Extract spawn pose and convert (relative) position to carla.Location
-        self.position = Location(
+        self.relative_position = Location(
             x=relative_spawn_pose.position.x,
             y=relative_spawn_pose.position.y,
             z=relative_spawn_pose.position.z
@@ -102,7 +109,7 @@ class IdealObjectSensor(ObjectSensor):
         roll_deg = math.degrees(roll_rad)
         pitch_deg = math.degrees(pitch_rad)
         yaw_deg = math.degrees(yaw_rad)
-        self.rotation = Rotation(pitch=pitch_deg, yaw=yaw_deg, roll=roll_deg)
+        self.relative_rotation = Rotation(pitch=pitch_deg, yaw=yaw_deg, roll=roll_deg)
         # Extract orientation
         self.relative_spawn_pose = relative_spawn_pose
 
@@ -145,18 +152,13 @@ class IdealObjectSensor(ObjectSensor):
         """
         return "sensor.pseudo.ideal_objects"
     
-    def check_visibility(self, sensor_location, target_location, sensor_pose, target_frame, timestamp, id): 
+    def check_visibility(self, sensor_location, target_location, target_pose_in_sensor_frame, timestamp): 
 
         # Calculate the euclidean distance between sensor and target 
         distance = sensor_location.distance(target_location)
 
-        # Store frame names and lookup transform from sensor to target
-        sensor_frame = self.get_prefix()
-        now = Time()
-        tf_sensor_target = self.tf_buffer.lookup_transform(target_frame, sensor_frame, now)
-
         # Calculate azimuth and elevation between sensor and target
-        azimuth_deg, elevation_deg, dx, dy, dz = self.calculate_azimuth_and_elevation(sensor_location, target_location, sensor_rotation, distance)
+        azimuth_deg, elevation_deg = self.calculate_azimuth_and_elevation(target_pose_in_sensor_frame, distance, timestamp)
 
         # Check if the target is inside the range and fov of the sensor
         if distance > self.range:
@@ -174,26 +176,25 @@ class IdealObjectSensor(ObjectSensor):
             #     print(f"Distance: {round(distance, 2)}, ID: {id}, dx: {round(dx,2)}, dy: {round(dy, 2)}, dz: {round(dz, 2)}, Azimuth: {round(azimuth_deg, 2)}, Elevation: {round(elevation_deg, 2)}")
             return True
     
-    def calculate_azimuth_and_elevation(self, sensor_location, target_location, sensor_rotation, distance):
 
-        # Calculate the vector from source to target
-        dx = target_location.x - sensor_location.x
-        dy = target_location.y - sensor_location.y
-        dz = target_location.z - sensor_location.z
+    def calculate_azimuth_and_elevation(self, target_pose_in_sensor_frame, distance, timestamp):
 
-        # Calculate azimuth in degrees
+        # Get location vaules of pose
+        dx = target_pose_in_sensor_frame.position.x
+        dy = target_pose_in_sensor_frame.position.y
+        dz = target_pose_in_sensor_frame.position.z
+
+        # Calculate azimuth
         azimuth_rad = math.atan(dy/dx)
         azimuth_deg = math.degrees(azimuth_rad)
 
-        # Calculate elevation in degrees
+        # Calculate elevation
         elevation_rad = math.asin(dz/distance)
         elevation_deg = math.degrees(elevation_rad)
 
-        # if distance < 50:
-            # print(f"Distance: {round(distance, 2)}, ID: {id}, dx: {round(dx,2)}, dy: {round(dy, 2)}, dz: {round(dz, 2)}, Azimuth: {round(azimuth_deg, 2)}, Elevation: {round(elevation_deg, 2)}")
-
-        return azimuth_deg, elevation_deg, dx, dy, dz
+        return azimuth_deg, elevation_deg
     
+
     def get_ros_transform(self, timestamp):
         if not self.relative_spawn_pose:
             self.node.logwarn("{}: No relative spawn pose defined.".format(self.get_prefix()))
@@ -219,7 +220,7 @@ class IdealObjectSensor(ObjectSensor):
         transform.transform.rotation.w = self.relative_spawn_pose.orientation.w
 
         return transform
-
+    
 
     def publish_tf(self, timestamp):
         transform =self.get_ros_transform(timestamp)
@@ -228,6 +229,7 @@ class IdealObjectSensor(ObjectSensor):
         except roscomp.exceptions.ROSException:
             if roscomp.ok():
                 self.node.logwarn("Sensor {} failed to send transform.".fromat(self.uid))
+
 
     def update(self, frame, timestamp):
         """
@@ -245,12 +247,30 @@ class IdealObjectSensor(ObjectSensor):
         ros_objects = ObjectArray()
         ros_objects.header = self.get_msg_header(frame_id="carla_map", timestamp=timestamp)
 
+        # sensor_frame = self.get_prefix()
+        # now = Time()
+        # print(f"Time: {now}")
+        # tf_carla_map_to_sensor = self.tf_buffer.lookup_transform(sensor_frame, 'carla_map')
+        # print(f"tf_carla_map_to_sensor: {tf_carla_map_to_sensor}")
         # Construct sensor location
+
         if not self.parent:
-            # Location and rotation for idealObjectSensor without vehicle parent
+            # Get location for idealObjectSensor without vehicle parent
+            sensor_location = self.relative_position
+
+            # Get pose of idealObjectSensor without vehicle parent
             sensor_pose = self.relative_spawn_pose
-            sensor_location = self.position
-            sensor_pose = self.relative_spawn_pose
+
+            # Transform sensor Pose to TransformStamped
+            tf_carla_map_to_sensor = TransformStamped()
+            tf_carla_map_to_sensor.transform.translation.x = sensor_pose.position.x
+            tf_carla_map_to_sensor.transform.translation.y = sensor_pose.position.y
+            tf_carla_map_to_sensor.transform.translation.z = sensor_pose.position.z
+            tf_carla_map_to_sensor.transform.rotation.x = sensor_pose.orientation.x
+            tf_carla_map_to_sensor.transform.rotation.y = sensor_pose.orientation.y
+            tf_carla_map_to_sensor.transform.rotation.z = sensor_pose.orientation.z
+            tf_carla_map_to_sensor.transform.rotation.w = sensor_pose.orientaiton.w
+
         else:
             """       
                 - Get the vehicle that the IdealObjectSensor is appended
@@ -262,11 +282,11 @@ class IdealObjectSensor(ObjectSensor):
             ego_vehicle_transform = ego_vehicle.carla_actor.get_transform()
 
             # Calculate location and orientation for idealObjectSensor including vehicle parent
-            sensor_location = self.position + ego_vehicle_transform.location
+            sensor_location = self.relative_position + ego_vehicle_transform.location
             sensor_orientation_degree = [
-                self.rotation.pitch + ego_vehicle_transform.rotation.pitch,
-                self.rotation.yaw + ego_vehicle_transform.rotation.yaw,
-                self.rotation.roll + ego_vehicle_transform.rotation.roll
+                self.relative_rotation.pitch + ego_vehicle_transform.rotation.pitch,
+                self.relative_rotation.yaw + ego_vehicle_transform.rotation.yaw,
+                self.relative_rotation.roll + ego_vehicle_transform.rotation.roll
             ]
 
             # Convert sensor orientation from degrees to rad
@@ -279,16 +299,15 @@ class IdealObjectSensor(ObjectSensor):
                 sensor_orientation_rad[2]
             )
 
-            # Create sensor pose for carla_map
-            sensor_pose = Pose()
-            sensor_pose.position.x = sensor_location.x
-            sensor_pose.position.y = sensor_location.y
-            sensor_pose.position.z = sensor_location.z
-            sensor_pose.orientation.x = sensor_orientation[0]
-            sensor_pose.orientation.y = sensor_orientation[1]
-            sensor_pose.orientation.z = sensor_orientation[2]
-            sensor_pose.orientation.w = sensor_orientation[3]
-
+            tf_carla_map_to_sensor = TransformStamped()
+            tf_carla_map_to_sensor.transform.translation.x = sensor_location.x
+            tf_carla_map_to_sensor.transform.translation.y = sensor_location.y
+            tf_carla_map_to_sensor.transform.translation.z = sensor_location.z
+            tf_carla_map_to_sensor.transform.rotation.x = sensor_orientation[0]
+            tf_carla_map_to_sensor.transform.rotation.y = sensor_orientation[1]
+            tf_carla_map_to_sensor.transform.rotation.z = sensor_orientation[2]
+            tf_carla_map_to_sensor.transform.rotation.w = sensor_orientation[3]
+        
         # Iterate over all dynamic actors
         for actor_id in self.actor_list.keys():
             
@@ -297,12 +316,17 @@ class IdealObjectSensor(ObjectSensor):
                 actor = self.actor_list[actor_id]
                 if isinstance(actor, Vehicle) or isinstance(actor, Walker):
                     
-                    # Get the location of the target
+                    # Get target location
                     target_location = actor.carla_actor.get_location()
-                    #TODO: get target frame
+
+                    # Get target pose in carla map
+                    target_pose_in_carla_map = actor.get_current_ros_pose()
+
+                    # Transform target pose from carla map to sensor frame
+                    target_pose_in_sensor_frame = do_transform_pose(target_pose_in_carla_map, tf_carla_map_to_sensor)
 
                     # Check visibility of the target
-                    if self.check_visibility(sensor_location, target_location, sensor_pose, target_frame, timestamp, actor.carla_actor.id):
+                    if self.check_visibility(sensor_location, target_location, target_pose_in_sensor_frame, timestamp):
                         ros_objects.objects.append(actor.get_object_info())
 
         # Iterate over all static vehicles
@@ -314,12 +338,39 @@ class IdealObjectSensor(ObjectSensor):
                     # Take only vehicles with bounding_box attribute set
                     if hasattr(vehicle, "bounding_box"):
                         
-                        # Get the location of the target
+                        # Get target location in carla map
                         target_location = vehicle.transform.location
-                        #TODO: get target frame
+
+                        # Get target rotation in carla map
+                        target_rotation_degree = [
+                            vehicle.transform.rotation.pitch,
+                            vehicle.transform.rotation.yaw,
+                            vehicle.transform.rotation.roll
+                        ]
+                        target_rotation_rad = np.radians(target_rotation_degree)
+
+                        # Convert rotation in rad to orientation in quaternion
+                        target_orientation = quaternion_from_euler(
+                            target_rotation_rad[0],
+                            target_rotation_rad[1],
+                            target_rotation_rad[2]
+                        )
+
+                        # Create target pose in carla map
+                        target_pose_in_carla_map = Pose()
+                        target_pose_in_carla_map.position.x = target_location.x
+                        target_pose_in_carla_map.position.y = target_location.y
+                        target_pose_in_carla_map.position.z = target_location.z
+                        target_pose_in_carla_map.orientation.x = target_orientation[0]
+                        target_pose_in_carla_map.orientation.y = target_orientation[1]
+                        target_pose_in_carla_map.orientation.z = target_orientation[2]
+                        target_pose_in_carla_map.orientation.w = target_orientation[3]
+
+                        # Transform target pos from carla map to sensor frame
+                        target_pose_in_sensor_frame = do_transform_pose(target_pose_in_carla_map, tf_carla_map_to_sensor)
 
                         # Check visibility of the target
-                        if self.check_visibility(sensor_location, target_location, sensor_pose, target_frame, timestamp, vehicle.id):
+                        if self.check_visibility(sensor_location, target_location, target_pose_in_sensor_frame, timestamp):
                             vehicle_obj = self._get_vehicle_from_environment_objects(vehicle, object_value)
                             ros_objects.objects.append(vehicle_obj)
 
