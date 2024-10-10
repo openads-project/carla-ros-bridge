833ce10a3ab1c20306ac4b654e33b95c5c686c44
Christian Geller <christian.geller@ika.rwth-aachen.de>
2024-10-10 16:40:34 +0200

    update tfs

--- carla_ros_bridge/src/carla_ros_bridge/tf_sensor.py
+++ carla_ros_bridge/src/carla_ros_bridge/tf_sensor.py
@@ -69,7 +69,7 @@ class TFSensor(PseudoActor):
         transform = None
         try:
             transform = self.parent.get_current_ros_transform()
-            transform.translation.z = 1.33  # autoshuttle height / 2
+            transform.translation.z = 0
         except AttributeError:
             # parent actor disappeared, do not send tf
             self.node.logwarn(
