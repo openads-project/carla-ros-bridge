e6e9c97ca9f7b85d84a84ea5a02aba4e3e7ff4b7
Christian Geller <christian.geller@ika.rwth-aachen.de>
2024-12-06 14:36:29 +0100

    update pip requirement installation

--- docker/custom.sh
+++ docker/custom.sh
@@ -1,10 +1,19 @@
 export DOCKER_ROS_FILES_PATH=/docker-ros/additional-files
+export PYTHON_VERSION_SHORT=$(python --version | awk -F'[ .]' '{print $2"."$3}')
 
 # Setup venv
 virtualenv -p python3 ./venv && touch ./venv/COLCON_IGNORE
+echo "source $WORKSPACE/venv/bin/activate" /root/.bashrc
+source ./venv/bin/activate
+
+# Install Python dependencies
+if [ "$ROS_DISTRO" = "noetic" ]; then
+    pip$PYTHON_VERSION_SHORT install -r $DOCKER_ROS_FILES_PATH/requirements-ros1.txt
+else
+    pip$PYTHON_VERSION_SHORT install -r $DOCKER_ROS_FILES_PATH/requirements-ros2.txt
+fi
 
 # Check if user provided CARLA PythonAPI. If not, download it as artifact from CARLA CI pipeline
-export PYTHON_VERSION_SHORT=$(python --version | awk -F'[ .]' '{print $2"."$3}')
 mkdir -p /opt/carla
 if [ -d "$DOCKER_ROS_FILES_PATH/artifacts/PythonAPI" ]; then
     mv $DOCKER_ROS_FILES_PATH/artifacts/PythonAPI /opt/carla/PythonAPI
@@ -15,6 +24,7 @@ else
     mv artifacts/PythonAPI /opt/carla
     rm -rf artifacts
 fi
+
 # Create a script to append necessary paths to PYTHONPATH and make .bashrc source it
 echo "export PYTHONPATH=\$PYTHONPATH:/opt/carla/PythonAPI/carla/dist/$(ls /opt/carla/PythonAPI/carla/dist | grep py$PYTHON_VERSION_SHORT.)" >> /opt/carla/setup.bash
 echo "export PYTHONPATH=\$PYTHONPATH:/opt/carla/PythonAPI/carla" >> /opt/carla/setup.bash
