84161be3799cd25f3facf95aca8bf6fab0fc7bc7
Jean-Pierre Busch <jean-pierre.busch@ika.rwth-aachen.de>
2024-12-12 11:17:25 +0100

    remove ROS 1 from ci and clean up dependencies

--- docker/custom.sh
+++ docker/custom.sh
@@ -1,31 +1,28 @@
-export DOCKER_ROS_FILES_PATH=/docker-ros/additional-files
-export PYTHON_VERSION_SHORT=$(python --version | awk -F'[ .]' '{print $2"."$3}')
-
 # Setup venv
 virtualenv -p python3 ./venv && touch ./venv/COLCON_IGNORE
 echo "source $WORKSPACE/venv/bin/activate" /root/.bashrc
 source ./venv/bin/activate
 
-# Install Python dependencies
-# if [ "$ROS_DISTRO" = "noetic" ]; then
-#     pip$PYTHON_VERSION_SHORT install -r $DOCKER_ROS_FILES_PATH/requirements-ros1.txt
-# else
-#     pip$PYTHON_VERSION_SHORT install -r $DOCKER_ROS_FILES_PATH/requirements-ros2.txt
-# fi
+pip install pep8==1.7.1 \
+            autopep8==2.0.4 \
+            cmake_format==0.6.11 \
+            pylint==3.0.3 \
+            transforms3d==0.4.1 \
+            pygame==2.5.2 \
+            pexpect==4.9.0 \
+            simple-pid==2.0.0 \
+            networkx==3.2.1 \
+            pyproj==3.6.1
 
-# Check if user provided CARLA PythonAPI. If not, download it as artifact from CARLA CI pipeline
+# Download PythonAPI as artifact from CARLA CI pipeline
 mkdir -p /opt/carla
-if [ -d "$DOCKER_ROS_FILES_PATH/artifacts/PythonAPI" ]; then
-    mv $DOCKER_ROS_FILES_PATH/artifacts/PythonAPI /opt/carla/PythonAPI
-else
-    mkdir -p /opt/carla
-    curl --location --output artifacts.zip "https://gitlab.ika.rwth-aachen.de/api/v4/projects/1645/jobs/artifacts/main/download?job=provide-python-api&job_token=$GIT_HTTPS_PASSWORD"
-    unzip artifacts.zip
-    mv artifacts/PythonAPI /opt/carla
-    rm -rf artifacts
-fi
+curl --location --output artifacts.zip "https://gitlab.ika.rwth-aachen.de/api/v4/projects/1645/jobs/artifacts/main/download?job=provide-python-api&job_token=$GIT_HTTPS_PASSWORD"
+unzip artifacts.zip
+mv artifacts/PythonAPI /opt/carla
+rm -rf artifacts
 
 # Create a script to append necessary paths to PYTHONPATH and make .bashrc source it
+export PYTHON_VERSION_SHORT=$(python --version | awk -F'[ .]' '{print $2"."$3}')
 echo "export PYTHONPATH=\$PYTHONPATH:/opt/carla/PythonAPI/carla/dist/$(ls /opt/carla/PythonAPI/carla/dist | grep py$PYTHON_VERSION_SHORT.)" >> /opt/carla/setup.bash
 echo "export PYTHONPATH=\$PYTHONPATH:/opt/carla/PythonAPI/carla" >> /opt/carla/setup.bash
 echo "source /opt/carla/setup.bash" >> /root/.bashrc
