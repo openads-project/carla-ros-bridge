4f6344260695a54b98f803d28c994c94f3116122
Jean-Pierre Busch <jean-pierre.busch@ika.rwth-aachen.de>
2024-12-13 13:36:06 +0100

    install python3.10 in custom.sh

--- docker/custom.sh
+++ docker/custom.sh
@@ -1,18 +1,33 @@
-# Setup venv
-virtualenv -p python3 ./venv && touch ./venv/COLCON_IGNORE
-echo "source $WORKSPACE/venv/bin/activate" /root/.bashrc
-source ./venv/bin/activate
+# install python3.10 as the carla-simulator currently only supports python3.10 and python3.8
 
-pip install pep8==1.7.1 \
-            autopep8==2.0.4 \
-            cmake_format==0.6.11 \
-            pylint==3.0.3 \
-            transforms3d==0.4.1 \
-            pygame==2.5.2 \
-            pexpect==4.9.0 \
-            simple-pid==2.0.0 \
-            networkx==3.2.1 \
-            pyproj==3.6.1
+# extend /etc/apt/sources.list with ubuntu 22 repositories to install python3.10
+echo "deb http://archive.ubuntu.com/ubuntu/ jammy main restricted" >> /etc/apt/sources.list
+echo "deb http://archive.ubuntu.com/ubuntu/ jammy-updates main restricted" >> /etc/apt/sources.list
+echo "deb http://archive.ubuntu.com/ubuntu/ jammy universe" >> /etc/apt/sources.list
+echo "deb http://archive.ubuntu.com/ubuntu/ jammy-updates universe" >> /etc/apt/sources.list
+echo "deb http://archive.ubuntu.com/ubuntu/ jammy multiverse" >> /etc/apt/sources.list
+echo "deb http://archive.ubuntu.com/ubuntu/ jammy-updates multiverse" >> /etc/apt/sources.list
+echo "deb http://archive.ubuntu.com/ubuntu/ jammy-backports main restricted universe multiverse" >> /etc/apt/sources.list
+echo "deb http://security.ubuntu.com/ubuntu/ jammy-security main restricted" >> /etc/apt/sources.list
+echo "deb http://security.ubuntu.com/ubuntu/ jammy-security universe" >> /etc/apt/sources.list
+echo "deb http://security.ubuntu.com/ubuntu/ jammy-security multiverse" >> /etc/apt/sources.list
+
+# install python3.10
+apt-get update
+apt-get install -y python3.10
+ln -sf /usr/bin/python3.10 /usr/bin/python3
+
+# install required packages with python3.10
+python3.10 -m pip install   pep8==1.7.1 \
+                            autopep8==2.0.4 \
+                            cmake_format==0.6.11 \
+                            pylint==3.0.3 \
+                            transforms3d==0.4.1 \
+                            pygame==2.5.2 \
+                            pexpect==4.9.0 \
+                            simple-pid==2.0.0 \
+                            networkx==3.2.1 \
+                            pyproj==3.6.1
 
 # Download PythonAPI as artifact from CARLA CI pipeline
 mkdir -p /opt/carla
@@ -22,8 +37,7 @@ mv artifacts/PythonAPI /opt/carla
 rm -rf artifacts
 
 # Create a script to append necessary paths to PYTHONPATH and make .bashrc source it
-export PYTHON_VERSION_SHORT=$(python --version | awk -F'[ .]' '{print $2"."$3}')
-echo "export PYTHONPATH=\$PYTHONPATH:/opt/carla/PythonAPI/carla/dist/$(ls /opt/carla/PythonAPI/carla/dist | grep py$PYTHON_VERSION_SHORT.)" >> /opt/carla/setup.bash
+echo "export PYTHONPATH=\$PYTHONPATH:/opt/carla/PythonAPI/carla/dist/$(ls /opt/carla/PythonAPI/carla/dist | grep py3.10.)" >> /opt/carla/setup.bash
 echo "export PYTHONPATH=\$PYTHONPATH:/opt/carla/PythonAPI/carla" >> /opt/carla/setup.bash
 echo "source /opt/carla/setup.bash" >> /root/.bashrc
 
