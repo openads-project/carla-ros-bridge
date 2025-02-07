b9c76df5e18b38da2a88db04fddbd75aca4da32d
Christian Geller <christian.geller@ika.rwth-aachen.de>
2025-02-07 14:45:21 +0100

    move back to additional-debs.txt

--- docker/custom.sh
+++ docker/custom.sh
@@ -18,7 +18,5 @@ echo "export PROJ_NETWORK=ON" >> /opt/carla/setup.bash
 apt-get remove -y python3-transforms3d
 pip install transforms3d==0.4.1
 
-apt-get install -y ros-$ROS_DISTRO-vision-opencv
-apt-get install -y ros-$ROS_DISTRO-rmw-zenoh-cpp
-apt-get install -y ros-$ROS_DISTRO-rqt-image-view
-apt-get install -y ros-$ROS_DISTRO-rqt-gui-py
+#apt-get install -y ros-$ROS_DISTRO-vision-opencv
+#apt-get install -y ros-$ROS_DISTRO-rmw-zenoh-cpp
