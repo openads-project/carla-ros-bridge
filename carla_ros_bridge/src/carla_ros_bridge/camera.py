b25ed1ef69e480c2bdf07dc1c5f63866ae10b5ce
Christian Geller <christian.geller@ika.rwth-aachen.de>
2025-07-11 15:29:22 +0200

    fix topic names

--- carla_ros_bridge/src/carla_ros_bridge/camera.py
+++ carla_ros_bridge/src/carla_ros_bridge/camera.py
@@ -78,10 +78,10 @@ class Camera(Sensor):
 
         if self.node.parameters['publish_compressed_images']:
             self.camera_image_publisher = node.new_publisher(CompressedImage, self.get_topic_prefix() +
-                                                            '/' + 'compressed', qos_profile=10)
+                                                            '/image/compressed', qos_profile=10)
         else:
             self.camera_image_publisher = node.new_publisher(Image, self.get_topic_prefix() +
-                                                            '/' + 'image', qos_profile=10)
+                                                            '/image', qos_profile=10)
 
     def destroy(self):
         super(Camera, self).destroy()
