935293a44d5db50f204e35bb2aa1d2159184b8b5
Jean-Pierre Busch <jean-pierre.busch@ika.rwth-aachen.de>
2025-08-15 10:54:27 +0200

    add params to resize manual control window

--- carla_manual_control/launch/carla_manual_control.launch.py
+++ carla_manual_control/launch/carla_manual_control.launch.py
@@ -15,6 +15,14 @@ def generate_launch_description():
             name='wireless_controller',
             default_value='False'
         ),
+        launch.actions.DeclareLaunchArgument(
+            name='width',
+            default_value='800'
+        ),
+        launch.actions.DeclareLaunchArgument(
+            name='height',
+            default_value='600'
+        ),
         launch_ros.actions.Node(
             package='carla_manual_control',
             executable='carla_manual_control',
@@ -24,7 +32,9 @@ def generate_launch_description():
             parameters=[
                 {
                     'role_name': launch.substitutions.LaunchConfiguration('role_name'),
-                    'wireless_controller': launch.substitutions.LaunchConfiguration('wireless_controller')
+                    'wireless_controller': launch.substitutions.LaunchConfiguration('wireless_controller'),
+                    'width': launch.substitutions.LaunchConfiguration('width'),
+                    'height': launch.substitutions.LaunchConfiguration('height')
                 }
             ]
         )
--- carla_manual_control/src/carla_manual_control/carla_manual_control.py
+++ carla_manual_control/src/carla_manual_control/carla_manual_control.py
@@ -788,7 +788,9 @@ def main(args=None):
     roscomp.init("manual_control", args=args)
 
     # resolution should be similar to spawned camera with role-name 'view'
-    resolution = {"width": 800, "height": 600}
+    width = roscomp.get_param("width", 800)
+    height = roscomp.get_param("height", 600)
+    resolution = {"width": width, "height": height}
 
     pygame.init()
     pygame.font.init()
