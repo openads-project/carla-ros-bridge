5e421626ce078da5f2f301907d5625a7090fc816
Christian Geller <christian.geller@ika.rwth-aachen.de>
2026-01-07 11:44:28 +0100

    add CARLA_CACHE_DIR
    Update default command
    
    Update CARLA_CACHE_DIR generation
    
    update comment

--- docker/custom.sh
+++ docker/custom.sh
@@ -24,7 +24,7 @@ fi
 
 export DOCKER_ROS_FILES_PATH=/docker-ros/additional-files
 
-# Check if user provided CARLA PythonAPI. If not, exit
+# Download PythonAPI as artifact from CARLA CI pipeline
 mkdir -p /opt/carla
 curl --location --output artifacts.zip "https://gitlab.ika.rwth-aachen.de/api/v4/projects/1645/jobs/artifacts/update/0.9.16/download?job=provide-carla-artifacts&job_token=$GIT_HTTPS_PASSWORD"
 unzip artifacts.zip
@@ -36,10 +36,16 @@ pyver=$(python3 -c "import sys; print(f'{sys.version_info.major}{sys.version_inf
 wheel=$(echo /opt/carla/PythonAPI/carla/dist/*${pyver}*.whl)
 pip install --no-cache-dir "$wheel" --break-system-packages
 
-# Create a script to append necessary paths to PYTHONPATH and make .bashrc source it
+# Create a script to append necessary paths to PYTHONPATH
 echo "export PYTHONPATH=\$PYTHONPATH:/opt/carla/PythonAPI/carla/agents" >> /opt/carla/setup.bash
 echo "export PYTHONPATH=\$PYTHONPATH:/opt/carla/PythonAPI/carla" >> /opt/carla/setup.bash
-echo "source /opt/carla/setup.bash" >> /root/.bashrc
 
 # Allow proj to automatically download remote grids to interpret the projection string in OpenDRIVE maps
 echo "export PROJ_NETWORK=ON" >> /opt/carla/setup.bash
+
+# Default file cache for CARLA client-side map files
+echo "export CARLA_CACHE_DIR=/tmp/carlaCache" >> /opt/carla/setup.bash
+echo "mkdir -p /tmp/carlaCache" >> /opt/carla/setup.bash
+
+# .bashrc sources the setup script
+echo "source /opt/carla/setup.bash" >> /root/.bashrc
