06a274208a5e0417387de5fc81bb403c3fdb75fe
Alexander Wilczynski <alexander.wilczynski@ika.rwth-aachen.de>
2026-01-14 09:52:48 +0100

    Modified ego vehicle launch example to fit new blueprint system

--- carla_spawn_objects/launch/carla_example_ego_vehicle.launch.py
+++ carla_spawn_objects/launch/carla_example_ego_vehicle.launch.py
@@ -9,13 +9,18 @@ def generate_launch_description():
         launch.actions.DeclareLaunchArgument(
             name='objects_definition_file',
             default_value='',
-            description='Single object definition file (legacy, use objects_definition_files for multiple files)'
+            description='Single object definition file (use objects_definition_files for multiple files)'
         ),
         launch.actions.DeclareLaunchArgument(
             name='objects_definition_files',
             default_value=get_package_share_directory(
                 'carla_spawn_objects') + '/config/objects.json',
             description='Comma-separated list of object definition files to load and merge'
+        ),
+        launch.actions.DeclareLaunchArgument(
+            name='blueprints_directory',
+            default_value='',
+            description='Directory containing blueprint JSON files (auto-detected if not specified)'
         ),
         launch.actions.DeclareLaunchArgument(
             name='role_name',
@@ -41,6 +46,7 @@ def generate_launch_description():
             launch_arguments={
                 'objects_definition_file': launch.substitutions.LaunchConfiguration('objects_definition_file'),
                 'objects_definition_files': launch.substitutions.LaunchConfiguration('objects_definition_files'),
+                'blueprints_directory': launch.substitutions.LaunchConfiguration('blueprints_directory'),
                 'spawn_point_ego_vehicle': launch.substitutions.LaunchConfiguration('spawn_point_ego_vehicle'),
                 'spawn_sensors_only': launch.substitutions.LaunchConfiguration('spawn_sensors_only')
             }.items()
