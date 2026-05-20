36625843b076da9df712b52c526de34061df5c13
Christian Geller <christian.geller@ika.rwth-aachen.de>
2026-05-20 21:55:39 +0000

    Fix artifact url

--- docker/custom.sh
+++ docker/custom.sh
@@ -26,7 +26,7 @@ export DOCKER_ROS_FILES_PATH=/docker-ros/additional-files
 
 # Download PythonAPI as artifact from CARLA CI pipeline
 mkdir -p /opt/carla
-curl --location --output artifacts.zip "https://gitlab.ika.rwth-aachen.de/api/v4/projects/1645/jobs/artifacts/ue5-ika/download?job=build-release-docker-image&job_token=$GIT_HTTPS_PASSWORD"
+curl --location --output artifacts.zip "https://gitlab.ika.rwth-aachen.de/api/v4/projects/1645/jobs/artifacts/ue5-ika/download?job=build-client-docker-image&job_token=$GIT_HTTPS_PASSWORD"
 unzip artifacts.zip
 mv artifacts/PythonAPI /opt/carla
 rm -rf artifacts
